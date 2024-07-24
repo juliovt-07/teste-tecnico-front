c7075caa2f832e16ee42dd907854cab870718437
Julio Cesar <juliocesaar.ma@gmail.com>
2024-07-24 15:56:34 -0300

    feat: adicionar gráfico estático

--- Vue/src/main.js
+++ Vue/src/main.js
@@ -4,9 +4,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueApexCharts from "vue3-apexcharts"
+
 
 const app = createApp(App)
 
+app.use(VueApexCharts)
 app.use(router)
 app.use(store)
 
--- Vue/src/views/InfoUsers.vue
+++ Vue/src/views/InfoUsers.vue
@@ -1,9 +1,13 @@
 <script setup>
 import VCard from '@/components/VCard.vue'
+import BarChart from '@/components/BarChart.vue'
 </script>
 
 <template>
   <VCard>
-    info
+    <h1 class="capitalize col-span-12 text-left mb-5 text-3xl font-bold text-gray-700">
+      info users
+    </h1>
+    <BarChart />
   </VCard>
 </template>
